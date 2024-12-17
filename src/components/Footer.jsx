e99028021278af6d47a8063a5d27e7964f7ed6c0
eidabdallah <eidabdallah971@gmail.com>
2024-12-17 21:49:25 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
           </div>
 
           <div className="col-md-4 footer-section">
-            <h4 className="fs-3 mb-4">AROUND THE WEB</h4>
+            <h4 className="fs-3 mb-4 leftSideP">AROUND THE WEB</h4>
             <div className="social-icons">
               <a href="#" className="social-link"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social-link"><i className="fab fa-twitter"></i></a>
@@ -19,7 +19,7 @@ export default function Footer() {
           </div>
 
           <div className="col-md-4 footer-section">
-            <h4 className="fs-3 mb-4">ABOUT FREELANCER</h4>
+            <h4 className="fs-3 mb-4 leftSideP">ABOUT FREELANCER</h4>
             <p className="fs-6 fw-bold">
               Freelance is a free to use, MIT licensed<br />
               Bootstrap theme created by <a href="https://startbootstrap.com/" className="footer-link">Start Bootstrap</a>.
